9c91a45b098dbec8c19e15a2bfce6a07e57eda14
Arnab Mondal <arnabmondal700@gmail.com>
2021-07-03 15:11:58 +0530

    bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { LoaderService } from './shared/loader/loader.service';
+import { LocationStrategy } from '@angular/common';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'voltaic';
   loading = false;
 
-  constructor(private loaderService: LoaderService) { }
+  constructor(private loaderService: LoaderService, private locationStrategy: LocationStrategy) { }
 
   ngOnInit() {
     this.loaderService.loader$.subscribe(value => {
       this.loading = value;
     });
+  }
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    if(location.href.slice(location.href.length-5,location.href.length)=='login')
+      history.forward();
   }
 }
--- src/app/post-auth/post-auth.component.ts
+++ src/app/post-auth/post-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
-import { HeaderComponent} from '../shared/header/header.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-post-auth',
@@ -9,11 +10,11 @@ import { HeaderComponent} from '../shared/header/header.component';
 })
 export class PostAuthComponent implements OnInit {
   @ViewChild(HeaderComponent)
- // private headerComp: HeaderComponent;
+  // private headerComp: HeaderComponent;
   public toggleLogoEvent = false;
   isDashboard = false;
   urlname = '';
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {// Show loading indicator
       }
@@ -21,32 +22,35 @@ export class PostAuthComponent implements OnInit {
         this.showBreadcrumb();
       }
       if (event instanceof NavigationError) { // Hide loading indicator // Present error to user
-          console.log(event.error);
+        console.log(event.error);
       }
-  });
+    });
   }
-  
+
   ngOnInit(): void {
+    setTimeout(() => {
+      this.toggleLogoEvent = false;
+    }, 100)
     this.showBreadcrumb();
   }
   showBreadcrumb() {
-    if(this.router.url.includes('dashboard'))
+    if (this.router.url.includes('dashboard'))
       this.isDashboard = true;
     else
-    this.isDashboard = false;
-    if(this.router.url.includes('job/reports')) {
+      this.isDashboard = false;
+    if (this.router.url.includes('job/reports')) {
       this.urlname = 'Job Reports';
     }
-    if(this.router.url.includes('manage/users')) {
+    if (this.router.url.includes('manage/users')) {
       this.urlname = 'Manage Users';
     }
-    if(this.router.url.includes('contact-leads')) {
+    if (this.router.url.includes('contact-leads')) {
       this.urlname = 'Contact Leads';
     }
   }
 
   toggleHeaderLogo(evnt: any) {
-   this.toggleLogoEvent = evnt.isShowLogo;
+    this.toggleLogoEvent = evnt.isShowLogo;
   }
 
 }

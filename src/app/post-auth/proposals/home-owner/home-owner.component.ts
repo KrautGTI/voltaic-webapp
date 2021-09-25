99dbc9b8f8bec1fb39d560ff093983d9f0e4057d
Nirupam Jana <nirupamjana.dev@gmail.com>
2021-09-25 22:46:37 +0530

    proposals page added issue fix

--- src/app/post-auth/proposals/home-owner/home-owner.component.ts
+++ src/app/post-auth/proposals/home-owner/home-owner.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
 import { of, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { liveSearch } from 'src/app/common/live-search.operator';
 import { HomeOwnerLabels } from 'src/app/shared/constants/proposal.constant';
 import { FormField } from 'src/app/shared/models/util.model';
@@ -10,7 +11,8 @@ import { FormField } from 'src/app/shared/models/util.model';
   templateUrl: './home-owner.component.html',
   styleUrls: ['./home-owner.component.scss'],
 })
-export class HomeOwnerComponent implements OnInit {
+export class HomeOwnerComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<boolean> = new Subject();
   @Input() public group: FormGroup = new FormGroup({});
   public label: { [key: string]: FormField } = HomeOwnerLabels;
   private searchTerm$ = new Subject<string>();
@@ -23,7 +25,21 @@ export class HomeOwnerComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.addCoSigner?.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((val) => {
+        // console.log('val=', val);
+      });
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
+  }
+
+  get addCoSigner(): AbstractControl | null {
+    return this.group.get('Add_Co_Signer');
+  }
 
   public onChangeState(term: string): void {
     this.searchTerm$.next(term);
